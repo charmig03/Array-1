533ae9f78b68614db34c8a029c8aebbd189345f2
charmii03 <charmigangani46@gmail.com>
2022-07-08 23:24:42 +0530

    js completed

---
+++ front11.js
@@ -0,0 +1,18 @@
+function front11(a, b){
+   let sections = [
+    a,
+    b
+  ]
+
+  let newA = [];
+
+  for (let section of sections) {
+    if (section.length == 0) {
+
+    } else {
+      newA.push(section[0]);
+    }
+  }
+
+  return newA;
+}
